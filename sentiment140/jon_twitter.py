943d0f07b01002832e5e74c66dd784e94dc0142c
Jon Bronson <jonbronson@gmail.com>
2018-04-08 14:04:43 -0600

    update twitter dataframe and script

--- sentiment140/jon_twitter.py
+++ sentiment140/jon_twitter.py
@@ -3,7 +3,8 @@ from pyspark.sql.session import SparkSession
 
 from pyspark.sql.types import DoubleType
 from pyspark.sql.types import IntegerType
-
+from pyspark.sql.functions import UserDefinedFunction
+from email.utils import parsedate_to_datetime
 
 sc = SparkContext("local", "Twitter")
 spark = SparkSession(sc)
@@ -19,6 +20,11 @@ df = df.withColumnRenamed('_c3', 'query')
 df = df.withColumnRenamed('_c4', 'user')
 df = df.withColumnRenamed('_c5', 'text')
 df = df.select(["tweetId", "date", "user", "text", "polarity", "query"])
+
+
+df = df.withColumn('date', UserDefinedFunction(lambda date: int(parsedate_to_datetime(date).timestamp()), IntegerType())('date'))
+
+
 
 df.write.parquet("twitter_training.parquet")
 
